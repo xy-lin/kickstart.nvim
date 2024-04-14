f5afbaab15fbc93eaacf10509405b9529b009b4f
xilin <xinyu_lin@mentor.com>
2024-04-14 10:06:06 +0100

    update mason-nvim-dap in debug lua to make it work in Windows 11

--- lua/custom/plugins/debug-cpp.lua
+++ lua/custom/plugins/debug-cpp.lua
@@ -30,10 +30,27 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
+    require('mason-nvim-dap').setup {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_setup = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
+        --'delve',
+        'codelldb',
+      },
+    }
     if not dap.adapters['codelldb'] then
       require('dap').adapters['codelldb'] = {
         type = 'server',
-        host = 'localhost',
+        --host = 'localhost',
         port = '${port}',
         executable = {
           command = 'codelldb',
